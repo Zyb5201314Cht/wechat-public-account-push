efd6d8c2265c45eba900ef44d3972aeae70e9ad6
Zyb5201314Cht <117655655+Zyb5201314Cht@users.noreply.github.com>
2022-11-08 01:11:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '涛涛蛋蛋',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oaLkX6tUC1zDXeTz9UqaZ7Am8I8Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'oSDIFbGHjGNGI_-UwFOrf2wWpdYdKMuRsWc3NIRBBx8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
       festivals: [
